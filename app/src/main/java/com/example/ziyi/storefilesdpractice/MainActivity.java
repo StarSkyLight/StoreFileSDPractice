d34d833ffd4b88dcd9352591e5629e98342af63a
StarSkyLight <stark_09@foxmail.com>
2016-09-07 21:40:34 +0800

    Initial commit

---
+++ app/src/main/java/com/example/ziyi/storefilesdpractice/MainActivity.java
@@ -0,0 +1,93 @@
+package com.example.ziyi.storefilesdpractice;
+
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final static String FileName = "students";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button_write = (Button)findViewById(R.id.button_write);
+        Button button_read = (Button)findViewById(R.id.button_read);
+
+        button_write.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        OutputStream outputStream = null;
+                        try{
+                            if(Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)){
+                                File file = Environment.getExternalStorageDirectory();
+                                File students = new File(file.getCanonicalPath()+"/"+FileName);
+
+                                FileOutputStream fileOutputStream = new FileOutputStream(students);
+                                outputStream = new BufferedOutputStream(fileOutputStream);
+                                String infor = "Name:ZhangSan  NO:001";
+                                try {
+                                    outputStream.write(infor.getBytes(StandardCharsets.UTF_8));
+                                }finally {
+                                    if(outputStream != null){
+                                        outputStream.close();
+                                    }
+                                }
+                            }
+                        }
+                        catch (Exception e){
+                            e.printStackTrace();
+                        }
+                    }
+                }
+        );
+
+        button_read.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        InputStream inputStream = null;
+                        try {
+                            if(Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)){
+                                File file = Environment.getExternalStorageDirectory();
+                                File students = new File(file.getCanonicalPath()+"/"+FileName);
+                                FileInputStream fileInputStream = new FileInputStream(students);
+                                inputStream = new BufferedInputStream(fileInputStream);
+
+                                int index;
+                                StringBuilder stringBuilder = new StringBuilder("");
+                                try{
+                                    while ((index = inputStream.read()) != -1){
+                                        stringBuilder.append((char)index);
+                                    }
+                                    Toast.makeText(MainActivity.this,stringBuilder.toString(),Toast.LENGTH_LONG).show();
+                                }finally {
+                                    if(inputStream != null){
+                                        inputStream.close();
+                                    }
+                                }
+                            }
+                        }
+                        catch (Exception e){
+                            e.printStackTrace();
+                        }
+                    }
+                }
+        );
+    }
+}
